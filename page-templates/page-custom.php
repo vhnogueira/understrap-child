59dce55d4b02d3e4ccef82c2e979b9227c77d862
Victor Hugo Nogueira de Castro <vhnogueira@users.noreply.github.com>
2020-03-19 20:32:24 -0300

    Create page-custom.php

---
+++ page-templates/page-custom.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Custom Page Template
+ *
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header(); ?>
+
+// Content Here
+
+<?php get_footer();
